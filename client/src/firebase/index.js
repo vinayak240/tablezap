7192e97d23f53e74db7ace4dcc0474f84580e827
vinayak240 <58338039+vinayak240@users.noreply.github.com>
2026-03-10 00:54:07 +0530

    Remove Public API Key

--- client/src/firebase/index.js
+++ client/src/firebase/index.js
@@ -1,17 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/storage";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCzg0riZ1suwzH4C5lKm5HqCCQAS1twN78",
-    authDomain: "a-fireb-1.firebaseapp.com",
-    databaseURL: "https://a-fireb-1.firebaseio.com",
-    projectId: "a-fireb-1",
-    storageBucket: "a-fireb-1.appspot.com",
-    messagingSenderId: "772036377409",
-    appId: "1:772036377409:web:7112fa85b64c8344e8ffc0",
-    measurementId: "G-L9TQEHP229"
-  };
-
+const firebaseConfig = process.env.FIRBASE_STORAGE_ENV
   firebase.initializeApp(firebaseConfig);
 
 const storage = firebase.storage();
